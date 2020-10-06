bfb3b8469fbcf09008ce7dce6490088330889730
Igor <kpe4ets@gmail.com>
2020-10-06 22:22:57 +0300

    hw3.1 subclasses done, but wierd

--- 3-STUDENTS/ipetrov/project/src/components/ancient.js
+++ 3-STUDENTS/ipetrov/project/src/components/ancient.js
@@ -1,12 +1,48 @@
+function createBItemTemplate(item) {
+    return `<div class="drop__box" data-id="${item.productId}">
+    <a class="drop__img" href="single.html"><img src="${item.productImg}" alt="#" width="72" height="85"></a>
+    <div class="drop__info">
+            <a href="single.html" class="drop__title">${item.productName}</a>
+            <img src="../src/assets/imgs/drop_cart/stars.png" alt="#">
+            <div class="drop__price"><span class="drop__count" data-id="${item.productId}">${item.amount}</span><span class="drop__span">
+            x
+            </span>$${item.productPrice}</div>
+        </div>
+        <button class="drop__cancel fas fa-times-circle" data-id="${item.productId}" name="remove">
+        </button>
+    </div>`;
+}
+
+function createCItemTemplate(item) {
+    return `<div class="catalog__item" id="${item.productId}">
+    <button 
+        class="catalog__bucket" 
+        name="add"
+        data-id="${item.productId}" 
+        data-price="${item.productPrice}" 
+        data-name="${item.productName}" 
+        data-image="${item.productImg}"
+    >
+        <img class="catalog__hidden_img" 
+        src="https://raw.githubusercontent.com/Eliseev88/geekbrains/3fdc76c4d5e84b1398b168e6239b8651dce01f6f/products/Forma_1_copy.svg" alt="#">
+        <span class="catalog__hidden_text">Add to Cart</span>
+    </button>
+    <div class="catalog__photo">
+        <img class="catalog__img" src="${item.productImg}" alt="#">
+    </div>
+    <div class="catalog__content">
+        <a class="catalog__name" href="#">${item.productName}</a>
+        <div class="catalog__price">$${item.productPrice}.00</div>
+    </div>
+</div>`
+}
+
 export default class ancientParent {
     constructor(url = '/basket.json', container = '#basket') {
         this.items = []; //+
         this.container = document.querySelector(container); //+
         this.url = 'https://raw.githubusercontent.com/kellolo/static/master/JSON' + url; //+
         this._init();
-        // this.basket = basket; // !!!!!c only
-        // this.shown = false; // !!!!!!!!!b only
-        // this.itemsContainer = document.querySelector('#basket-items'); // !!!!b only
     }
 
     _init () {
@@ -24,55 +60,13 @@ export default class ancientParent {
             .then(data => data.json()) // JSON >>> Obj/Array
     }
 
-    // _handleActions() { // для каждого класса свой
-    //     document.querySelector('#basket-toggler').addEventListener('click', () => {
-    //         this.shown = !this.shown;
-    //         this.container.classList.toggle('invisible');
-    //     });
-
-    //     this.container.addEventListener('click', ev => {
-    //         if (ev.target.name == 'remove') {
-    //             this._remove(ev.target.dataset.id);
-    //         }
-    //     })
-    // }
-
-    // _render() { чорд его знает, надо всем. но разное
-    //     let str = '';
-    //     this.items.forEach(item => {
-    //         str += `<div class="drop__box" data-id="${item.productId}">
-    //                     <a class="drop__img" href="single.html"><img src="${item.productImg}" alt="#" width="72" height="85"></a>
-    //                     <div class="drop__info">
-    //                         <a href="single.html" class="drop__title">${item.productName}</a>
-    //                         <img src="../src/assets/imgs/drop_cart/stars.png" alt="#">
-    //                         <div class="drop__price"><span class="drop__count" data-id="${item.productId}">${item.amount}</span><span class="drop__span">
-    //                         x
-    //                         </span>$${item.productPrice}</div>
-    //                     </div>
-    //                     <button class="drop__cancel fas fa-times-circle" data-id="${item.productId}" name="remove">
-    //                     </button>
-    //                 </div>`;
-    //     });
-    //     this.itemsContainer.innerHTML = str;
-    // }
-
-//     add(product) { только в баскете
-//         let find = this.items.find(el => el.productId == product.productId);
-//             if (!find) {
-//                 this.items.push(Object.assign(product, { amount: 1 }));
-//             } else {
-//                 find.amount++;
-//             }
-//         this._render();
-//     }
-
-//     _remove(id) {
-//         let find = this.items.find(el => el.productId == id);
-//         if (find.amount > 1) {
-//             find.amount--;
-//         } else {
-//             this.items.splice(this.items.indexOf(find), 1);
-//         }
-//         this._render();
-//     }
+    _render() {
+        let str = '';
+        this.items.forEach(item => {
+            str += (this.url.includes('basket'))?
+            createBItemTemplate(item):createCItemTemplate(item);
+        });
+        this.url.includes('basket')?
+        this.itemsContainer.innerHTML = str:this.container.innerHTML = str;
+    }
 };
--- 3-STUDENTS/ipetrov/project/src/components/basket.js
+++ 3-STUDENTS/ipetrov/project/src/components/basket.js
@@ -8,24 +8,6 @@ export default class Basket extends ancientParent {
         this.itemsContainer = document.querySelector('#basket-items');
     }
 
-    _render() {
-        let str = '';
-        this.items.forEach(item => {
-            str += `<div class="drop__box" data-id="${item.productId}">
-                        <a class="drop__img" href="single.html"><img src="${item.productImg}" alt="#" width="72" height="85"></a>
-                        <div class="drop__info">
-                            <a href="single.html" class="drop__title">${item.productName}</a>
-                            <img src="../src/assets/imgs/drop_cart/stars.png" alt="#">
-                            <div class="drop__price"><span class="drop__count" data-id="${item.productId}">${item.amount}</span><span class="drop__span">
-                            x
-                            </span>$${item.productPrice}</div>
-                        </div>
-                        <button class="drop__cancel fas fa-times-circle" data-id="${item.productId}" name="remove">
-                        </button>
-                    </div>`;
-        });
-        this.itemsContainer.innerHTML = str;
-    }
     _handleActions() {
         document.querySelector('#basket-toggler').addEventListener('click', () => {
             this.shown = !this.shown;
